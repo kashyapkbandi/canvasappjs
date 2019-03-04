b34abeffaae7dd356088a1d2d9d85e425a20ba32
kashyapkbandi <kkbandi@outlook.com>
2019-03-04 17:25:52 +0530

    For customer

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,14 @@
-<script type="text/javascript" src="https://.salesforce.com/canvas/sdk/js/44.0/canvas-all.js"/>
-    // callback to access the OAuth access token and context object
-    Sfdc.canvas(function() {
-        // Save the token
-        Sfdc.canvas.oauth.token(window.signedRequestJson.oauthToken);
-        window.alert("hello, " + window.signedRequestJson.context.user.fullName);
-          console.log("hello, " + window.signedRequestJson.context.user.fullName);
-    });
 
+// const sfdc = require('sfdc');
 
+document.getElementById('submitButton').addEventListener("click",function(e){
 
+  console.log("Hello");
+  e.preventDefault();
+});
 
-document.getElementById('submitButton').addEventListener("click",function(){
-
-  alert('Hello');
+Sfdc.canvas(function() {
+    // Save the token
+    sfdc.canvas.oauth.token(window.signedRequestJson.oauthToken);
+    window.alert("hello, " + window.signedRequestJson.context.user.fullName);
 });
--- server.js
+++ server.js
@@ -1,25 +1,12 @@
+const express = require('express');
 
+var app = express();
+const port = process.env.PORT || 3000;
+// middleware
+// __dirname - stores path to directory
+app.use(express.static(__dirname + '/assets'));
 
-  const path = require('path');
-  const express = require('express');
-  var app = express();
-  const port = process.env.PORT || 3000;
-  // __dirname - stores path to directory
-  app.use(express.static(path.join(__dirname,'/assets')));
-
-  app.set('view engine','hbs');
-
-//setting up route
-
-app.get('',(req,res)=>{
-res.render('index', {
-title:'Weather App',
-author:'Kashyap Kalakbandi'
-
-})
+app.get('/',(req,res)=>{
+  res.send('hello Express');
 });
-  app.listen(3000,()=>{
-  //  console.log(__dirname);
-    console.log('Server is up on port 3000');
-
-  });
+app.listen(port);
