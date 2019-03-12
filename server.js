7d48aea8a34e0dd60bf5b41385a354291358465f
kashyapkbandi <kkbandi@outlook.com>
2019-03-12 17:22:14 +0530

    Added page 2 and fixed the hbs issue

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const path = require('path');
+const hbs = require('hbs'); 
+
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -10,14 +13,15 @@ const viewPath = path.join(__dirname,'./templates/views')
 // tell express to use templates instead of views. 
 app.set('views',viewPath);
 
-app.get('/',(req,res)=>{
-  res.render('about',{title:'ABout Me !',author:'Kashyap Kalakbandi ',
-  viewName:'index'  });
+app.get('',(req,res)=>{
+res.render('index',{viewName:'page2'  });
 });
 
 app.get('/page2',(req,res)=>{
-  res.render('about',{title:'ABout Me !',author:'Kashyap Kalakbandi ',
-  viewName:'index'  });
+res.render('page2',{viewName:'index'  });
 });
+app.set('view engine','hbs');
 
-app.listen(port);
+app.listen(port, ()=>{
+  console.log('Server is up and Running on port 3000');
+});
