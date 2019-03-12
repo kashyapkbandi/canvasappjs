4403e33ed7f954e3784ab60203fbda10169f7a52
kashyapkbandi <kkbandi@outlook.com>
2019-03-12 17:42:02 +0530

    adremoved some stuff

--- server.js
+++ server.js
@@ -5,21 +5,30 @@ const hbs = require('hbs');
 
 var app = express();
 const port = process.env.PORT || 3000;
+
 // middleware
 // __dirname - stores path to directory
+
 app.use(express.static(__dirname + '/assets'));
-const viewPath = path.join(__dirname,'./templates/views')
+const viewPath = path.join(__dirname,'./templates/views');
+
 console.log(viewPath);
 // tell express to use templates instead of views. 
+
 app.set('views',viewPath);
 
 app.get('',(req,res)=>{
-res.render('index',{viewName:'page2'  });
+res.render('index');
 });
 
+
 app.get('/page2',(req,res)=>{
-res.render('page2',{viewName:'index'  });
+res.render('page2');
 });
+
 app.set('view engine','hbs');
+
+
+
 
 app.listen(port);
