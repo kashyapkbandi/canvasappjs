e35c5316bf5d3c0023f89d963f3e6fdf7770742e
kashyapkbandi <kkbandi@outlook.com>
2019-03-12 16:59:19 +0530

    Added page2

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,5 @@
-
-// const sfdc = require('sfdc');
-
 document.getElementById('submitButton').addEventListener("click",function(e){
 
   console.log("Hello");
   e.preventDefault();
-});
-
-Sfdc.canvas(function() {
-    // Save the token
-    sfdc.canvas.oauth.token(window.signedRequestJson.oauthToken);
-    window.alert("hello, " + window.signedRequestJson.context.user.fullName);
 });
