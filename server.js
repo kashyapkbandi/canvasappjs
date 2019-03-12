57fb893469f75fd47eea70d023177a3dcd32d4cb
kashyapkbandi <kkbandi@outlook.com>
2019-03-12 17:06:37 +0530

    added some logic

--- server.js
+++ server.js
@@ -5,8 +5,19 @@ const port = process.env.PORT || 3000;
 // middleware
 // __dirname - stores path to directory
 app.use(express.static(__dirname + '/assets'));
+const viewPath = path.join(__dirname,'./templates/views')
+
+// tell express to use templates instead of views. 
+app.set('views',viewPath);
 
 app.get('/',(req,res)=>{
-  res.send('hello Express');
+  res.render('about',{title:'ABout Me !',author:'Kashyap Kalakbandi ',
+  viewName:'index'  });
 });
+
+app.get('/page2',(req,res)=>{
+  res.render('about',{title:'ABout Me !',author:'Kashyap Kalakbandi ',
+  viewName:'index'  });
+});
+
 app.listen(port);
