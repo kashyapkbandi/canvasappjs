e1610dc4efa64850169018c3760333121a4202d8
kashyapkbandi <kkbandi@outlook.com>
2019-09-05 01:32:29 +0530

    Signed-off-by: kashyapkbandi <kkbandi@outlook.com>

--- assets/app.js
+++ assets/app.js
@@ -3,15 +3,15 @@ document.getElementById('submitButton').addEventListener('click',onLogin);
 
 
 function onLogin(identity) {
-  
+
 var targetDiv = document.querySelector(SFIDWidget.config.target);	
 
 var avatar = document.createElement('a'); 
-  avatar.href = "javascript:showIdentityOverlay();";
+avatar.href = "javascript:showIdentityOverlay();";
 
 
 var img = document.createElement('img'); 
-  img.src = identity.photos.thumbnail; 
+img.src = identity.photos.thumbnail; 
 img.className = "sfid-avatar";
 
 var username = document.createElement('span'); 
@@ -19,7 +19,7 @@ username.innerHTML = identity.username;
 username.className = "sfid-avatar-name";
 
 var iddiv = document.createElement('div'); 
-  iddiv.id = "sfid-identity";
+iddiv.id = "sfid-identity";
 
 avatar.appendChild(img);
 avatar.appendChild(username);
