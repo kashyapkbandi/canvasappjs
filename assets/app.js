3a544b9450d1bb6b51eabc82013225355b3981e8
kashyapkbandi <kkbandi@outlook.com>
2019-09-05 01:28:40 +0530

    Signed-off-by: kashyapkbandi <kkbandi@outlook.com>

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,32 @@
-document.getElementById('submitButton').addEventListener('click',handleBack);
+document.getElementById('submitButton').addEventListener('click',onLogin);
 
 
-function handleBack(e){
 
-  console.log("Hello");
+function onLogin(identity) {
+  
+var targetDiv = document.querySelector(SFIDWidget.config.target);	
 
-  e.preventDefault();
- }   
+var avatar = document.createElement('a'); 
+  avatar.href = "javascript:showIdentityOverlay();";
+
+
+var img = document.createElement('img'); 
+  img.src = identity.photos.thumbnail; 
+img.className = "sfid-avatar";
+
+var username = document.createElement('span'); 
+username.innerHTML = identity.username;
+username.className = "sfid-avatar-name";
+
+var iddiv = document.createElement('div'); 
+  iddiv.id = "sfid-identity";
+
+avatar.appendChild(img);
+avatar.appendChild(username);
+iddiv.appendChild(avatar);		
+
+targetDiv.innerHTML = '';
+targetDiv.appendChild(iddiv);	
+
+
+}
