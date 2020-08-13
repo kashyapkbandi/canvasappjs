54123b2edc30ed4a651b211863a5f33cea59b4e5
kashyapkbandi <kkbandi@outlook.com>
2020-08-13 12:51:02 +0530

    added console.log better

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ jsPath: __dirname+'/assets/app.js'
 // POST ENDPOINT TO RECEIVE THE PAYLOAD AND DISPLAY IT IN THE FORM
 
 app.post('/poster',(req,res)=>{
-  console.log(req.body);
+  console.log('POSTER LOG----'+req.body);
   res.send(req.body);
   });
   
